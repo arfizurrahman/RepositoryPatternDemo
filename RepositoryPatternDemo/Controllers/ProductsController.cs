7e7937b5ff2cf23346681de13010eb72a40f63bf
Arfiz <arfizur.rahman@vivacomsolutions.com>
2021-07-04 22:53:35 +0600

    Implemented CRUD endpoints

---
+++ RepositoryPatternDemo/Controllers/ProductsController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RepositoryPatternDemo.Entities;
+using RepositoryPatternDemo.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepositoryPatternDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _context.Products.Where(x => x.Rating == 5).Take(1).ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}", Name = "ProductById")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(Product product)
+        {
+            _context.Products.Add(product);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtRoute("ProductById", new { id = product.Id },
+                product);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            _context.Products.Remove(product);
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(Product
+            product)
+        {
+            _context.Products.Update(product);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ RepositoryPatternDemo/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryPatternDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepositoryPatternDemo.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
